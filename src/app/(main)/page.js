9f6e8381f0b99be2d1a01cbeea7511267a26198d
Moajjem Hossain <moajjem441@gmail.com>
2026-05-03 22:32:59 +0600

    categories part updated

--- src/app/(main)/page.js
+++ src/app/(main)/page.js
@@ -6,7 +6,7 @@ import { Children } from "react";
 
 const getData =async()=>{
   const res =await fetch("https://openapi.programming-hero.com/api/news/categories");
-  const data = res.json();
+  const data = await res.json();
 
   return data.data;
 }
@@ -19,18 +19,25 @@ const allCategories=await getData();
         
         <div className="grid grid-cols-12 gap-2 mx-auto container  py-4">
 
-        <div className="grid col-span-3">
-          <h2>All Categories</h2>
+        <div className="grid col-span-3  gap-2">
+          <h2 className="font-bold my-2">All Categories</h2>
+
+         <ul>
+           {
+            allCategories.news_category.map(category=><li className="bg-gray-500 mb-2 text-center p-2" key={category.category_id}
+            >{category.category_name}</li>)
+          }
+         </ul>
 
         </div>
 
 
-        <div className="grid col-span-6">
+        <div className="grid col-span-6  gap-2">
           <h2>News HOme</h2>
         </div>
 
 
-            <div className="grid col-span-3">
+            <div className="grid col-span-3  gap-2">
               <h1>Login with</h1>
             </div>
         </div>
