1a17c8098f7043935903c1132238aa3f2bcf84ff
Moajjem Hossain <moajjem441@gmail.com>
2026-05-03 18:38:38 +0600

    breaking news part done

--- src/components/BreakingNews.jsx
+++ src/components/BreakingNews.jsx
@@ -1,7 +1,7 @@
 import Marquee from "react-fast-marquee";
 
 
-const news = [
+const breakingNews = [
   { id: 1, title: "Global Tech Companies Invest Heavily in AI Innovation" },
   { id: 2, title: "Climate Change Summit 2026 Brings New Global Agreements" },
   { id: 3, title: "Breakthrough in Renewable Energy Promises Cheaper Power" },
@@ -13,9 +13,12 @@ const news = [
 
 const BreakingNews = () => {
     return (
-        <div>
-            <Marquee>
-                
+        <div className="flex justify-center items-center  container mx-auto bg-gray-200 gap-3">
+            <button className="btn btn-accent   mx-2">Latest News</button>
+            <Marquee pauseOnHover={true}>
+                {
+                    breakingNews.map(news=><span className="mx-4 text-black" key={news.id}>{news.title}</span>)
+                }
          </Marquee>
         </div>
     );
