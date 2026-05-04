66cd7a8a74c1dafbe6ecfd6124d0b05c8dc7dbb9
Moajjem Hossain <moajjem441@gmail.com>
2026-05-04 21:50:22 +0600

    news card part updated

--- src/components/Homepage/CardNews.jsx
+++ src/components/Homepage/CardNews.jsx
@@ -1,12 +1,14 @@
 import { format } from 'date-fns';
 import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 import { CiBookmark, CiShare2 } from 'react-icons/ci';
+import { FaRegEye, FaStar } from 'react-icons/fa';
 
 const CardNews = ({ news }) => {
     console.log(news)
     return (
-        <div class="card bg-base-100  shadow-sm py-2">
+        <div class="card bg-base-100  shadow-sm py-2 p-2">
             {/* author info */}
             <div className='bg-gray-500 flex justify-between items-center p-2'>
                 <div className='flex gap-4 items-center'>
@@ -38,15 +40,33 @@ const CardNews = ({ news }) => {
             </div>
             <figure>
                 <Image
-                 src={news?.image_url}
-                 alt={news.author?.name || "news_image"}
-                 width={200}
-                 height={200}
+                    src={news?.image_url}
+                    alt={news.author?.name || "news_image"}
+                    width={200}
+                    height={200}
                 ></Image>
             </figure>
             <p className='my-2 p-2'>{news.details}</p>
 
             {/* footer fo card  */}
+
+            <div className='flex justify-between items-center'>
+                <div className='flex gap-2 justify-between items-center'>
+                    <div className='flex gap-2 justify-between items-center'>
+                        <div><FaStar className='text-orange-300' /></div>
+                        <p>{news.rating.number}</p>
+                    </div>
+                    <div className='flex gap-2 justify-between items-center'>
+                        <div><FaRegEye /></div>
+                        <p>{news.total_view}</p>
+                    </div>
+                </div>
+
+                <div>
+                    <Link href={`/news/${news._id}`}><button
+                        className='btn btn-active'>See more</button></Link>
+                </div>
+            </div>
         </div>
 
     );
