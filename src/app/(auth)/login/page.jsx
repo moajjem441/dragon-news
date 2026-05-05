bae352c8aa2a23bb50178967d61f9e45d05021fc
Moajjem Hossain <moajjem441@gmail.com>
2026-05-05 23:39:45 +0600

    text field added

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -5,6 +5,12 @@ const LogInPage = () => {
     return (
         <div className='container flex justify-center items-center h-[85vh]'>
             <h1>Log in</h1>
+
+            <fieldset className="fieldset">
+  <legend className="fieldset-legend">What is your name?</legend>
+  <input type="text" className="input" placeholder="Type here" />
+  <p className="label">Optional</p>
+</fieldset>
             
         </div>
     );
