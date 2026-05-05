dd6f0e06ab88e660ceb27112f825be14f807f40f
Moajjem Hossain <moajjem441@gmail.com>
2026-05-05 23:46:10 +0600

    card added in log in page

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -7,10 +7,29 @@ const LogInPage = () => {
             <h1>Log in</h1>
 
             <fieldset className="fieldset">
-                <legend className="fieldset-legend">What is your name?</legend>
-                <input type="text" className="input" placeholder="Type here" />
+                <legend className="fieldset-legend">Write your email</legend>
+                <input type="email" className="input" name='email' placeholder="Type here" />
                 <p className="label">Optional</p>
             </fieldset>
+            
+            <fieldset className="fieldset">
+                <legend className="fieldset-legend">Write your email</legend>
+                <input type="email" className="input" name='email' placeholder="Type here" />
+                <p className="label">Optional</p>
+            </fieldset>
+
+
+
+<div className="card w-96 bg-base-100 card-md shadow-sm">
+  <div className="card-body">
+    <h2 className="card-title">Medium Card</h2>
+    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+    <div className="justify-end card-actions">
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+            
 
         </div>
     );
